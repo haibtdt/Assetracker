02fb88ae8da4dc8b0f0f2bc38c60848d090289e0
Hai Bui <haibui19872015@gmail.com>
2015-10-05 18:02:21 +0700

    [ADD] new project

---
+++ Assetracker/Assetracker.h
@@ -0,0 +1,19 @@
+//
+//  Assetracker.h
+//  Assetracker
+//
+//  Created by SB 8 on 10/5/15.
+//  Copyright © 2015 vn.haibui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Assetracker.
+FOUNDATION_EXPORT double AssetrackerVersionNumber;
+
+//! Project version string for Assetracker.
+FOUNDATION_EXPORT const unsigned char AssetrackerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Assetracker/PublicHeader.h>
+
+
